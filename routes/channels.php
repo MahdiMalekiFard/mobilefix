204c5a5fc4d3b7281cb7fb4b55717fdc4180a5e8
Mahdi_new <mmf1344py@gmail.com>
2025-09-17 13:30:11 +0330

    remove logs .

--- routes/channels.php
+++ routes/channels.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Log;
 
-// Test if this file is even loaded
-Log::info('Laravel 12: channels.php file loaded', [
-    'timestamp' => now()->toIso8601String(),
-]);
 
 /*
 |--------------------------------------------------------------------------
@@ -25,12 +21,5 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 
 // Private channel for conversations - Laravel 12 compatible - TEMPORARY: Allow all users
 Broadcast::channel('conversation.{conversationId}', function ($user, $conversationId) {
-    \Illuminate\Support\Facades\Log::info('Laravel 12: Channel auth called - ALLOWING ALL', [
-        'user_id' => $user->id,
-        'conversation_id' => $conversationId,
-        'timestamp' => now()->toIso8601String(),
-    ]);
-    
-    // TEMPORARY: Allow all authenticated users
     return true;
 });
